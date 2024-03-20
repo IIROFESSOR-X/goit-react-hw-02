1b895b4ecb264e3959830980073c7a59c794243c
IIROFESSOR-X <IIROFESSOR@gmail.com>
2024-03-20 22:29:01 +0200

    fix 1

--- src/components/feedback/Feedback.jsx
+++ src/components/feedback/Feedback.jsx
@@ -1,5 +1,9 @@
 import css from './Feedback.module.css';
+
 export const Feedback = ({ reviews, total }) => {
+
+  const positivePercentage = total > 0 ? Math.round(((reviews.good + reviews.neutral) / total) * 100) : 0;
+
   return (
     <ul className={css.list}>
       <li>
@@ -16,10 +20,7 @@ export const Feedback = ({ reviews, total }) => {
       </li>
       {total > 0 && (
         <li>
-          <p>
-            Positive:{' '}
-            {Math.round(((reviews.good + reviews.neutral) / total) * 100)}%
-          </p>
+          <p>Positive: {positivePercentage}%</p>
         </li>
       )}
     </ul>
